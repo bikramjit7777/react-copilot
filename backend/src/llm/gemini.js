d3f0361d38aa375613c92fac93131f046ab72de5
bikramjit7777 <bikramjit7777@gmail.com>
2026-04-11 15:22:53 -0400

    LLM file for gemini model

---
+++ backend/src/llm/gemini.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+export const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash",
+  temperature: 0.2,
+  maxRetries: 2
+});
